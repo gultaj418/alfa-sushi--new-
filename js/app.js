b163916443d5db4608d89ae170e1566aedf3e53c
gultaj418 <gultacat@code.edu.az>
2022-02-15 18:19:04 +0400

    search

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ $(function () {
     autostart: true,
     property: "value",
     onComplete: null,
-    duration: 20000,
+    duration: 30000,
     padding: 10,
     marquee_class: ".marquee",
     container_class: ".simple-marquee-container",
--- js/marquee.js
+++ js/marquee.js
@@ -299,7 +299,7 @@
     autostart: true,
     property: "value",
     onComplete: null,
-    duration: 20000,
+    duration: 30000,
     padding: 10,
     marquee_class: ".marquee",
     container_class: ".simple-marquee-container",
